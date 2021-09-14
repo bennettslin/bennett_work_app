5a3eec303c00cdff86f8b66fd842825bf7f480c1
Bennett Lin <bennettslin@gmail.com>
2021-09-13 23:15:37 -0700

    Log dominoes work.

--- src/tasks/bobtailDominoes/remaining/next/index.js
+++ src/tasks/bobtailDominoes/remaining/next/index.js
@@ -1,18 +1,18 @@
 export default [
     {
-        taskName: `Home button logo. Make sure it's centred properly when selected.`,
-        workedHours: 0,
-        neededHours: 2,
+        taskName: `Home button logo. Make sure it's centred properly when selected. Have text "The Bobtail Yearlings present"`,
+        workedHours: 2,
+        neededHours: 3,
+    },
+    {
+        taskName: `Create images for illegal and legal moves. Have default board image.`,
+        workedHours: 3,
+        neededHours: 5,
     },
     {
         taskName: `Containers for embedded images.`,
         workedHours: 0,
         neededHours: 2,
-    },
-    {
-        taskName: `Create images for illegal and legal moves, and chords for scoring.`,
-        workedHours: 0,
-        neededHours: 3,
     },
     {
         taskName: `Add images to rules.`,
@@ -25,19 +25,29 @@ export default [
         neededHours: 5,
     },
     {
+        taskName: `Pick font and colour for headings.`,
+        workedHours: 0,
+        neededHours: 1,
+    },
+    {
         taskName: `Figure out styling for selected menu buttons, and header and footer relative to page in general.`,
         workedHours: 0,
         neededHours: 4,
     },
     {
-        taskName: `Pick a heading font.`,
+        taskName: `Create image files for rules and patent.`,
         workedHours: 0,
         neededHours: 1,
     },
     {
-        taskName: `Create image files for rules and patent.`,
+        taskName: `Get back to Nelson.`,
         workedHours: 0,
         neededHours: 1,
+    },
+    {
+        taskName: `Create images for scoring.`,
+        workedHours: 0,
+        neededHours: 3,
     },
     {
         taskName: `Have setup for creating daily puzzle pngs and svgs. Need random piece generator. Include documentation.`,
--- src/tasks/bobtailYearlings/remaining/next/index.js
+++ src/tasks/bobtailYearlings/remaining/next/index.js
@@ -1,17 +1,7 @@
 export default [
     {
-        taskName: `Have Page component own blog index list and article section list.`,
-        workedHours: 0,
-        neededHours: 1,
-    },
-    {
-        taskName: `Get back to Nelson.`,
-        workedHours: 0,
-        neededHours: 1,
-    },
-    {
-        taskName: `Decide on meta description text for each page.`,
-        workedHours: 0,
+        taskName: `Get dimensions from svg itself.`,
+        workedHours: 1,
         neededHours: 2,
     },
     {
@@ -23,6 +13,11 @@ export default [
         taskName: `Finish music getting worse essay. Post on social media, and pin tweet.`,
         workedHours: 1,
         neededHours: 10,
+    },
+    {
+        taskName: `Decide on meta description text for each page.`,
+        workedHours: 0,
+        neededHours: 2,
     },
     {
         taskName: `Redo band bio, put on BY website.`,
