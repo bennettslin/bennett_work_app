2bbab4a50827dd795812fc983f9a411f1758e692
Bennett Lin <bennettslin@gmail.com>
2022-04-29 00:21:05 -0700

    Move some tasks.

--- src/tasks/yearlingsBobtail/remaining/next/index.js
+++ src/tasks/yearlingsBobtail/remaining/next/index.js
@@ -21,6 +21,52 @@ export default [
         workedHours: 213,
     },
 
+    // Monday.
+    {
+        taskName: `New monitor and wall speaker side cutouts. Adjust mic to be at yIndex 4?`,
+        workedHours: 0,
+        neededHours: 3,
+    },
+    // Tuesday.
+    {
+        taskName: `Club decoration cutouts: curtains, flood lights, Christmas lights. Presences already added.`,
+        workedHours: 0,
+        neededHours: 4,
+    },
+    // Wednesday.
+    {
+        taskName: `Couch shelf panel. Presence already added.`,
+        workedHours: 0,
+        neededHours: 2,
+    },
+    {
+        taskName: `Hospital bed interior panel. Presence already added.`,
+        workedHours: 0,
+        neededHours: 2,
+    },
+    // Thursday.
+    {
+        taskName: `Bennett books and water glass. Presences already added.`,
+        workedHours: 0,
+        neededHours: 2,
+    },
+    {
+        taskName: `Sita shrine. Presence already added.`,
+        workedHours: 0,
+        neededHours: 3,
+    },
+    // Friday.
+    {
+        taskName: `Chinese paintings in Odin mat and stairs scenes. Presences already added.`,
+        workedHours: 0,
+        neededHours: 3,
+    },
+    {
+        taskName: `A laughing Buddha statue in Odin stairs scene. Presence already added.`,
+        workedHours: 0,
+        neededHours: 3,
+    },
+
     // Nice to have.
     {
         taskName: `Create upsell modal that renders on load based on stored state. Use existing Popup component.`,
