2b7848c1f0cc42593190a664b8671f71f8028ea7
Bennett Lin <bennettslin@gmail.com>
2023-09-25 02:47:56 -0700

    Log Friday, Saturday, and Sunday hours.

--- src/tasks/yearlingsBobtail/completed/general/index.js
+++ src/tasks/yearlingsBobtail/completed/general/index.js
@@ -1,7 +1,7 @@
 export default [
     {
         taskName: `More annotation edits.`,
-        workedHours: 26,
+        workedHours: 42,
     },
     {
         taskName: `Revise Odin scenes to use dream backdrops.`,
