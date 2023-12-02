ff1b15e8d34cb54643fe5e142b291d157730227b
Bennett Lin <bennettslin@gmail.com>
2023-12-02 01:46:19 -0800

    Log Friday work.

--- src/tasks/yearlingsBobtail/completed/general/index.js
+++ src/tasks/yearlingsBobtail/completed/general/index.js
@@ -1,7 +1,7 @@
 export default [
     {
         taskName: `More annotation edits.`,
-        workedHours: 246,
+        workedHours: 254,
     },
     {
         taskName: `Revise Odin scenes to use dream backdrops.`,
